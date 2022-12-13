a2b5dfe2372ee2257450b6a069130ed79d7401bb
Nguyễn <tdinhnguyen279@gmail.com>
2022-12-13 15:29:52 +0700

    docs: :memo: setup project

---
+++ src/post/type/post.types.ts
@@ -0,0 +1,11 @@
+/* eslint-disable prettier/prettier */
+export class CreatePostTypes {
+  content: string;
+  title: string;
+}
+
+export class UpdatePostTypes {
+  id: number;
+  content: string;
+  title: string;
+}
